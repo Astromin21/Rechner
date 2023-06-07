08099ed717b7b3b93ded887010cc5c754f16ced7
Astromin21 <docgit@gmail.com>
2023-06-07 14:53:21 +0200

    ende vor Urlaub

--- main.js
+++ main.js
@@ -7,28 +7,49 @@ function init() {
 
 function keylistener() {
     let knopf = document.querySelector('.eingabe')
+    let eingabe = [];
     knopf.addEventListener('click', (e) => {
         let key = e.target;
-        let zahl = key.dataset.value;
+        eingabe.push(key.dataset.value);
+        let zahl = [eingabe.join("")]
         let action = key.dataset.action;
+        let rechnung = [];
+        console.log(eingabe);
+        console.log(zahl);
 
-        if (action === "+")
+        if (action === '+'){
+            rechnung.push((zahl[0]));
+            //let ergebnis = parseInt(zahl[0]) + parseInt(zahl[1]);
+            console.log(rechnung);
+        }
 
-        if (action === "-")
-
-        if (action === "*")
-
-        if (action === "/")
+        
+        /*
+            (action === '-') ||
+            (action === '*') ||
+            (action === '/') ||
+            (action === '=')) {
+            console.log("Ein Operator wurde angeklickt");
+        
+        if (action === '.') {
+            console.log("Komma wurde angeklickt");
+        }
+        if (action === 'clear') {
+            action = "";
+        } 
+        if (!isNaN(zahl)){
+            eingabe.push(zahl);
+        }
+        */
+        updateDisplay(zahl)
+        
     })
-}
-
-function rechnen(){
 
 }
 
-function updateDisplay(action) {
+
+function updateDisplay(eingabe) {
     let anzeige = document.querySelector('#ergebnis');
-    let eingabe = [];
     anzeige.innerHTML = eingabe;
 }
 
