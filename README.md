88edd9370a67910e1c470638bc9b489809423dfe
Dominik <docgit@gmail.comgit commit -m first>
2023-06-13 09:06:38 +0200

    adding float calc README.md edit CSS update

---
+++ README.md
@@ -0,0 +1,11 @@
+### Rechner mit Eingabe per Click ###
+
+- Rechner mit Plus-, Minus-, Division- und Multiplikationfunktion
+- Gerechnet kann mit Ganz- und Kommazahlen
+- Gecleared wird mit dem Button 'AC'
+
+```
+keylistener()
+```
+- erkennt welcher Knopf im html geclickt wurde
+- Überprüft Eingabe der Operatoren und
--- main.js
+++ main.js
@@ -2,25 +2,29 @@
 
 let zahlen = [];
 
-function init() {
-    keylistener()
-}
 //---------------------Listerner + Abfrage Operator------------------------------------------------
 function keylistener() {
     let knopf = document.querySelector('.eingabe')
     let eingabe = [];
 
     knopf.addEventListener('click', (e) => {
-        let key = e.target;
+        let key = e.target; if (operator === '=') {
+            updateDisplay(zahlen[0]);
+            zahlen = [zahlen[0]];
+        }
         eingabe.push(key.dataset.value);
         let zahl = [eingabe.join("")];
-        zahl = parseInt(zahl)
         let operator = key.dataset.action;
-
-        console.log(zahl);
-
+        let comma = key.dataset.float;
+        //console.log(zahl);
         updateDisplay(zahl);
 
+        if (comma === 'comma') {
+            eingabe.push('.');
+            zahl = [eingabe.join("")];
+            updateDisplay(zahl);
+            comma = '';
+        }
         if (!operator == "") {
             zahlen.push(zahl);
             eingabe = [];
@@ -31,35 +35,31 @@ function keylistener() {
             zahl = [];
             operator = "";
             updateDisplay(zahl);
-            
-        } else if (zahlen.length >= 3) {
+        }
+        if (zahlen.length >= 3) {
             speicher();
         }
-        if(operator == '='){
-            console.log('fertig', zahlen[0]);
+        if (operator === '=') {
+            updateDisplay(zahlen[0]);
+            zahlen = [zahlen[0]];
         } else if (operator) {
-            zahlen.push(operator); 
-        }   
+            zahlen.push(operator);
+        }
     })
 }
 //---------------------Eingabespeicher + Rechenstart------------------------------------------------
 function speicher() {
     let zahl1 = parseFloat(zahlen[0]);
-    let operator = zahlen[1]        
-
+    let operator = zahlen[1]
     let zahl2 = parseFloat(zahlen[2]);
-
     let erg = rechnen(zahl1, zahl2, operator)
     zahlen = [erg]
-    console.log("erg", zahlen);
+    //console.log("erg", zahlen);
     updateDisplay(erg);
-
 }
 //---------------------Rechnen----------------------------------------------------------------------
 function rechnen(zahl1, zahl2, operator) {
-
     if (operator == '+') {
-        console.log("rechnet");
         return zahl1 + zahl2
     }
     if (operator == '-') {
@@ -71,13 +71,11 @@ function rechnen(zahl1, zahl2, operator) {
     if (operator == '/') {
         return zahl1 / zahl2
     }
-
 }
 //---------------------Display Ausgabe----------------------------------------------------------------------       
-
 function updateDisplay(eingabe) {
     let anzeige = document.querySelector('#ergebnis');
     anzeige.innerHTML = eingabe;
 }
 
-init()
+keylistener()
